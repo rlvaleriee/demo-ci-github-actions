476c3310e0e0f31a784e7d97c38e1f3fd2311876
rlvaleriee <valeromul@gmail.com>
2025-11-14 19:38:23 -0600

    Rompo una prueba para ver fallo en CI

--- test/suma.test.js
+++ test/suma.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const { suma } = require('../index'); 
 
 try { 
-    assert.strictEqual(suma(2, 3), 5); 
+    assert.strictEqual(suma(2, 3), 6); 
     assert.strictEqual(suma(-1, 1), 0); 
     assert.strictEqual(suma(0, 0), 0); 
     console.log("Todas las pruebas pasaron correctamente."); 
